ae79deffb818b140eebf6e25a7f9fbd72ec59fa3
ppedvLukasK <lukask@ppedv.de>
2022-03-07 15:49:06 +0100

    Create M001_Basics_und_Tipps.sql

---
+++ M001_Basics_und_Tipps.sql
@@ -0,0 +1,86 @@
+-- ********************************* OPTIONEN UND EINSTELLUNGEN ****************************************************
+
+
+-- Explorer: beliebig andocken mit drag&drop
+
+-- Explorer: pin & unpin
+-- new Project: File -> New... -> Project
+-- new Query: Rechtsklick auf Query -> New Query
+
+-- Rechtsclick auf Results: Copy with Headers (z.B. für Tabelle -> Excel)
+
+-- Farbindikatoren in Scrollbar: grün/gelb/rot/blau
+-- grün: gespeichert
+-- gelb: neu
+-- rot: Fehler
+-- blau: hier steht gerade der Cursor
+
+-- Zeilennummern: Tools -> Options... -> Text Editor -> All Languages -> General -> Line Numbers
+
+-- Word Wrap: Tools -> Options... -> Text Editor -> All Languages -> General -> Word Wrap
+
+-- Scrollbar Map Mode: Tools -> Options... -> Text Editor -> All Languages -> Scroll Bars -> Use map mode for vertical scrollbar
+
+
+
+-- **************************** HäUFIGE KEY COMBINATIONS/SHORTCUTS ******************************************
+/*
+
+F5 - Execute
+STRG + E - execute highlighted Query
+
+F7 - öffnet Object Explorer Details
+
+STRG + R - öffnet/schließt Results
+
+STRG + Leer - Vorschläge
+
+STRG + K, STRG + C - Auskommentieren
+STRG + K, STRG + U - Uncomment
+
+STRG + N - neuer Query-Tab
+STRG + Tab - zwischen einzelnen Tabs springen/Tab auswählen
+
+STRG + SHIFT + U - Uppercase
+STRG + SHIFT + L - Lowercase
+
+STRG + SHIFT + V - toggle zwischen letzten Einträgen im Zwischenspeicher
+
+STRG + F - Text finden
+STRG + G - bestimmte Zeile finden
+STRG + H - finden und ersetzen
+
+STRG + W - Wort markieren
+
+SHIFT + END - bis zum Ende der Zeile markieren
+SHIFT + Pos1 - bis zum Anfang der Zeile markieren
+SHIFT + UPARROW/DOWNARROW - Zeile rauf/runter markieren
+
+ALT + SHIFT + ARROW - mehrere Zeilen editieren
+
+ALT + ARROW UP/DOWN - derzeitige Zeile oder alle markierten Zeilen eine Zeile nach oben/unten bewegen
+
+*/
+
+-- richtige Datenbank verwenden! USE DBNAME oder oben im Auswahlfenster einstellen
+USE Northwind
+
+-- Kommentar einzeilig
+
+/*
+
+	mehrzeilige
+	Kommentare
+
+*/
+
+
+-- STRG + R = Resultfenster ein-/ausblenden
+
+
+--mehrzeilge 
+--Kommentare sind auch so
+--möglich
+
+-- Tastenkombination STRG + K, STRG + C (comment)
+-- STRG + K, STRG + U (uncomment)
